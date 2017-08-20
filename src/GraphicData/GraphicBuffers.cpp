65b16eba08e5eca97fc9f138817d72d20dbbba63
Daniel Mevoroth Quach <daniel@quach.info>
2017-08-20 02:22:38 +0200

    Support RT/DS heap type

--- src/GraphicData/GraphicBuffers.cpp
+++ src/GraphicData/GraphicBuffers.cpp
@@ -53,12 +53,12 @@ void GraphicBuffers::Initialize(_In_ Device& DeviceObj)
 	for (uint32_t GraphicBufferIndex = 0; GraphicBufferIndex < GRAPHIC_BUFFER_NO_DEPTH_COUNT; ++GraphicBufferIndex)
 	{
 		swprintf(GraphicBufferName, ETERNAL_ARRAYSIZE(GraphicBufferName), L"Graphic Buffer %d", GraphicBufferIndex);
-		_GraphicBuffersHeap[GraphicBufferIndex]		= CreateHeap(DeviceObj, HEAP_TYPE_TEXTURE, 1, true, false, false, false);
+		_GraphicBuffersHeap[GraphicBufferIndex]		= CreateHeap(DeviceObj, HEAP_TYPE_RENDER_TARGET_DEPTH_STENCIL, 1, true, false, false, false);
 		_GraphicBuffersResource[GraphicBufferIndex]	= CreateResource(DeviceObj, *_GraphicBuffersHeap[GraphicBufferIndex], GraphicBufferName, RESOURCE_DIMENSION_TEXTURE_2D, GraphicBufferFormats[GraphicBufferIndex], TEXTURE_RENDER_TARGET, Width, Height, 1, 1, TRANSITION_UNDEFINED);
 		GetArray()[GraphicBufferIndex]				= _GraphicBuffersResource[GraphicBufferIndex]->CreateRenderTargetView(DeviceObj, *_GraphicBuffersDescriptorHeap, GraphicBufferFormats[GraphicBufferIndex]);
 	}
 
-	_GraphicBuffersHeap[GRAPHIC_BUFFER_DEPTH]		= CreateHeap(DeviceObj, HEAP_TYPE_TEXTURE, 1, true, false, false, false);
+	_GraphicBuffersHeap[GRAPHIC_BUFFER_DEPTH]		= CreateHeap(DeviceObj, HEAP_TYPE_RENDER_TARGET_DEPTH_STENCIL, 1, true, false, false, false);
 	_GraphicBuffersResource[GRAPHIC_BUFFER_DEPTH]	= CreateResource(DeviceObj, *_GraphicBuffersHeap[GRAPHIC_BUFFER_DEPTH], L"Depth Buffer", RESOURCE_DIMENSION_TEXTURE_2D, GraphicBufferFormats[GRAPHIC_BUFFER_DEPTH], TEXTURE_DEPTH_STENCIL, Width, Height, 1, 1, TRANSITION_UNDEFINED);
 	GetArray()[GRAPHIC_BUFFER_DEPTH]				= _GraphicBuffersResource[GRAPHIC_BUFFER_DEPTH]->CreateDepthStencilView(DeviceObj, *_DepthStencilDescriptorHeap, GraphicBufferFormats[GRAPHIC_BUFFER_DEPTH]);
 }
