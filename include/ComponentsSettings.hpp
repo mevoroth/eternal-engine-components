90d017f0cff81de6c905420ba12e14e4f1ce0194
Daniel 'Mevoroth' Quach <darkskana@gmail.com>
2021-06-19 17:53:08 +0200

    Update project

--- include/ComponentsSettings.hpp
+++ include/ComponentsSettings.hpp
@@ -1,7 +1,9 @@
 #ifndef _COMPONENTS_SETTINGS_HPP_
 #define _COMPONENTS_SETTINGS_HPP_
 
+#ifndef ETERNAL_USE_DXMATH_TYPES
 #define ETERNAL_USE_DXMATH_TYPES
+#endif
 
 #include <cstdint>
 #include <vector>
