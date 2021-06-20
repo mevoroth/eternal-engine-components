f73c583a24a22115be2100c12741071307e8781a
Daniel 'Mevoroth' Quach <darkskana@gmail.com>
2021-06-20 14:54:46 +0200

    Graphics api refactoring

--- include/GraphicData/RenderTargetCollection.hpp
+++ include/GraphicData/RenderTargetCollection.hpp
@@ -1,42 +1,42 @@
 #ifndef _RENDER_TARGET_COLLECTION_HPP_
 #define _RENDER_TARGET_COLLECTION_HPP_
 
-namespace Eternal
-{
-	namespace Graphics
-	{
-		enum Format;
-		class RenderTarget;
-	}
-
-	namespace GraphicData
-	{
-		using namespace Eternal::Graphics;
-
-		class RenderTargetCollection
-		{
-		public:
-			/**
-			 * Rendertargets + Depth
-			 */
-			RenderTargetCollection(_In_ int Width, _In_ int Height, _In_ int RenderTargetsCount, _In_ const Format* Formats, _In_ bool CreateDepth = false);
-			/**
-			 * Only Depth
-			 */
-			RenderTargetCollection(_In_ int Width, _In_ int Height);
-			virtual ~RenderTargetCollection();
-
-			RenderTarget** GetRenderTargets();
-			int GetRenderTargetsCount() const;
-
-			RenderTarget* GetDepthStencilRenderTarget();
-
-		private:
-			RenderTarget* _DepthStencilRenderTarget = nullptr;
-			RenderTarget** _RenderTargets = nullptr;
-			int _RenderTargetsCount = 0;
-		};
-	}
-}
+//namespace Eternal
+//{
+//	namespace Graphics
+//	{
+//		enum Format;
+//		class RenderTarget;
+//	}
+//
+//	namespace GraphicData
+//	{
+//		using namespace Eternal::Graphics;
+//
+//		class RenderTargetCollection
+//		{
+//		public:
+//			/**
+//			 * Rendertargets + Depth
+//			 */
+//			RenderTargetCollection(_In_ int Width, _In_ int Height, _In_ int RenderTargetsCount, _In_ const Format* Formats, _In_ bool CreateDepth = false);
+//			/**
+//			 * Only Depth
+//			 */
+//			RenderTargetCollection(_In_ int Width, _In_ int Height);
+//			virtual ~RenderTargetCollection();
+//
+//			RenderTarget** GetRenderTargets();
+//			int GetRenderTargetsCount() const;
+//
+//			RenderTarget* GetDepthStencilRenderTarget();
+//
+//		private:
+//			RenderTarget* _DepthStencilRenderTarget = nullptr;
+//			RenderTarget** _RenderTargets = nullptr;
+//			int _RenderTargetsCount = 0;
+//		};
+//	}
+//}
 
 #endif
--- src/GraphicData/RenderTargetCollection.cpp
+++ src/GraphicData/RenderTargetCollection.cpp
@@ -2,73 +2,73 @@
 
 #include "Graphics/RenderTargetFactory.hpp"
 
-using namespace Eternal::GraphicData;
+//using namespace Eternal::GraphicData;
 
-RenderTargetCollection::RenderTargetCollection(_In_ int Width, _In_ int Height, _In_ int RenderTargetsCount, _In_ const Format* Formats, _In_ bool CreateDepth /*= false*/)
-{
-	ETERNAL_ASSERT(RenderTargetsCount > 0);
-
-	_RenderTargetsCount = RenderTargetsCount;
-	_RenderTargets = new RenderTarget*[RenderTargetsCount];
-
-	//for (int RenderTargetIndex = 0; RenderTargetIndex < RenderTargetsCount; ++RenderTargetIndex)
-	//{
-	//	_RenderTargets[RenderTargetIndex] = CreateRenderTarget(Width, Height, Formats[RenderTargetIndex]);
-	//}
-
-	//if (CreateDepth)
-	//{
-	//	_DepthStencilRenderTarget = CreateDepthStencilRenderTarget(Width, Height);
-	//	ETERNAL_ASSERT(_DepthStencilRenderTarget);
-	//}
-	//else
-	//{
-	//	_DepthStencilRenderTarget = nullptr;
-	//}
-}
-
-RenderTargetCollection::RenderTargetCollection(_In_ int Width, _In_ int Height)
-	: _RenderTargetsCount(0)
-	, _RenderTargets(nullptr)
-{
-	//_DepthStencilRenderTarget = CreateDepthStencilRenderTarget(Width, Height);
-	ETERNAL_ASSERT(_DepthStencilRenderTarget);
-}
-
-RenderTargetCollection::~RenderTargetCollection()
-{
-	ETERNAL_ASSERT(_DepthStencilRenderTarget || _RenderTargets); // Corruption
-
-	if (_DepthStencilRenderTarget)
-	{
-		delete _DepthStencilRenderTarget;
-		_DepthStencilRenderTarget = nullptr;
-	}
-
-	if (_RenderTargets)
-	{
-		for (int RenderTargetIndex = 0; RenderTargetIndex < _RenderTargetsCount; ++RenderTargetIndex)
-		{
-			delete _RenderTargets[RenderTargetIndex];
-			_RenderTargets[RenderTargetIndex] = nullptr;
-		}
-		delete[] _RenderTargets;
-		_RenderTargets = nullptr;
-	}
-}
-
-RenderTarget** RenderTargetCollection::GetRenderTargets()
-{
-	return _RenderTargets;
-}
-
-int RenderTargetCollection::GetRenderTargetsCount() const
-{
-	return _RenderTargetsCount;
-}
-
-RenderTarget* RenderTargetCollection::GetDepthStencilRenderTarget()
-{
-	return _DepthStencilRenderTarget;
-}
+//RenderTargetCollection::RenderTargetCollection(_In_ int Width, _In_ int Height, _In_ int RenderTargetsCount, _In_ const Format* Formats, _In_ bool CreateDepth /*= false*/)
+//{
+//	ETERNAL_ASSERT(RenderTargetsCount > 0);
+//
+//	_RenderTargetsCount = RenderTargetsCount;
+//	_RenderTargets = new RenderTarget*[RenderTargetsCount];
+//
+//	//for (int RenderTargetIndex = 0; RenderTargetIndex < RenderTargetsCount; ++RenderTargetIndex)
+//	//{
+//	//	_RenderTargets[RenderTargetIndex] = CreateRenderTarget(Width, Height, Formats[RenderTargetIndex]);
+//	//}
+//
+//	//if (CreateDepth)
+//	//{
+//	//	_DepthStencilRenderTarget = CreateDepthStencilRenderTarget(Width, Height);
+//	//	ETERNAL_ASSERT(_DepthStencilRenderTarget);
+//	//}
+//	//else
+//	//{
+//	//	_DepthStencilRenderTarget = nullptr;
+//	//}
+//}
+//
+//RenderTargetCollection::RenderTargetCollection(_In_ int Width, _In_ int Height)
+//	: _RenderTargetsCount(0)
+//	, _RenderTargets(nullptr)
+//{
+//	//_DepthStencilRenderTarget = CreateDepthStencilRenderTarget(Width, Height);
+//	ETERNAL_ASSERT(_DepthStencilRenderTarget);
+//}
+//
+//RenderTargetCollection::~RenderTargetCollection()
+//{
+//	ETERNAL_ASSERT(_DepthStencilRenderTarget || _RenderTargets); // Corruption
+//
+//	if (_DepthStencilRenderTarget)
+//	{
+//		delete _DepthStencilRenderTarget;
+//		_DepthStencilRenderTarget = nullptr;
+//	}
+//
+//	if (_RenderTargets)
+//	{
+//		for (int RenderTargetIndex = 0; RenderTargetIndex < _RenderTargetsCount; ++RenderTargetIndex)
+//		{
+//			delete _RenderTargets[RenderTargetIndex];
+//			_RenderTargets[RenderTargetIndex] = nullptr;
+//		}
+//		delete[] _RenderTargets;
+//		_RenderTargets = nullptr;
+//	}
+//}
+//
+//RenderTarget** RenderTargetCollection::GetRenderTargets()
+//{
+//	return _RenderTargets;
+//}
+//
+//int RenderTargetCollection::GetRenderTargetsCount() const
+//{
+//	return _RenderTargetsCount;
+//}
+//
+//RenderTarget* RenderTargetCollection::GetDepthStencilRenderTarget()
+//{
+//	return _DepthStencilRenderTarget;
+//}
 
