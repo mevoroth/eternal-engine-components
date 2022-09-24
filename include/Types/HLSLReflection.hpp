99027e407eaf306737a75c51dad25d851cb38c21
Daniel Quach <daniel@quach.info>
2022-09-24 12:13:56 +0200

    array hlsl reflection

--- include/Types/HLSLReflection.hpp
+++ include/Types/HLSLReflection.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Macros/Macros.hpp"
+#include "Types/Types.hpp"
+
 namespace Eternal
 {
 	namespace Types
@@ -8,6 +11,36 @@ namespace Eternal
 		struct Vector3;
 		struct Vector2;
 		struct Matrix4x4;
+
+		template<typename HLSLType, typename PaddingType, int Count>
+		class HLSLArray
+		{
+			template<typename HLSLType>
+			class HLSLArrayEntry
+			{
+			public:
+
+				HLSLType& operator=(const HLSLType& NewValue)
+				{
+					Value = NewValue;
+					return Value;
+				}
+
+			private:
+				HLSLType Value;
+				PaddingType _Pad;
+			};
+		public:
+
+			HLSLArrayEntry<HLSLType>& operator[](int Index)
+			{
+				ETERNAL_ASSERT(Index < Count);
+				return Entries[Index];
+			}
+
+		private:
+			HLSLArrayEntry<HLSLType> Entries[Count];
+		};
 	}
 }
 
@@ -17,10 +50,14 @@ using float3	= Eternal::Types::Vector3;
 using float2	= Eternal::Types::Vector2;
 using uint		= unsigned int;
 
-#define HLSL_BEGIN_STRUCT(StructName)		namespace Eternal { \
-												namespace HLSL { \
-													struct StructName {
+#define HLSL_BEGIN_STRUCT(StructName)			namespace Eternal { \
+													namespace HLSL { \
+														struct StructName {
 
-#define HLSL_END_STRUCT(StructName)					}; \
-												} \
-											}
+#define HLSL_END_STRUCT(StructName)						}; \
+													} \
+												}
+
+#define HLSL_ARRAY_VEC1(Type, VariableName, Count)	Eternal::Types::HLSLArray<Type, Eternal::Types::Vector3, Count> VariableName
+#define HLSL_ARRAY_VEC2(Type, VariableName, Count)	Eternal::Types::HLSLArray<Type, Eternal::Types::Vector2, Count> VariableName
+#define HLSL_ARRAY_VEC3(Type, VariableName, Count)	Eternal::Types::HLSLArray<Type, float, Count> VariableName
