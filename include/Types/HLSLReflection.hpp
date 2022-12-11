be2c8514545669672bda6d42ccf3048b8a8fb690
Daniel Quach <daniel@quach.info>
2022-12-11 20:45:39 +0100

    uint4 reflection

--- include/Types/HLSLReflection.hpp
+++ include/Types/HLSLReflection.hpp
@@ -48,6 +48,7 @@ using float4x4	= Eternal::Types::Matrix4x4;
 using float4	= Eternal::Types::Vector4;
 using float3	= Eternal::Types::Vector3;
 using float2	= Eternal::Types::Vector2;
+using uint4		= Eternal::Types::Uint4;
 using uint		= unsigned int;
 
 #define HLSL_BEGIN_STRUCT(StructName)			namespace Eternal { \
