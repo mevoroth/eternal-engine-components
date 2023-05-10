6fa4f50b4812957ae58d51b51bf51f5f1e8b6cba
Daniel Quach <darkskana@gmail.com>
2023-05-10 17:48:52 +0200

    added extend() and changed aabb default value

--- include/Mesh/AxisAlignedBoundingBox.hpp
+++ include/Mesh/AxisAlignedBoundingBox.hpp
@@ -13,12 +13,13 @@ namespace Eternal
 		public:
 			const Vector3& GetMin() const;
 			const Vector3& GetMax() const;
-			void SetMin(_In_ const Vector3& Min);
-			void SetMax(_In_ const Vector3& Max);
+			void SetMin(_In_ const Vector3& InMin);
+			void SetMax(_In_ const Vector3& InMax);
+			void Extend(_In_ const Vector3& InPosition);
 
 		private:
-			Vector3 _Min = -Vector3::One;
-			Vector3 _Max = Vector3::One;
+			Vector3 _Min = Vector3::PositiveInfinity;
+			Vector3 _Max = Vector3::NegativeInfinity;
 		};
 	}
 }
--- src/Mesh/AxisAlignedBoundingBox.cpp
+++ src/Mesh/AxisAlignedBoundingBox.cpp
@@ -1,23 +1,33 @@
 #include "Mesh/AxisAlignedBoundingBox.hpp"
 
-using namespace Eternal::Components;
-
-const Vector3& AxisAlignedBoundingBox::GetMin() const
+namespace Eternal
 {
-	return _Min;
-}
+	namespace Components
+	{
+		const Vector3& AxisAlignedBoundingBox::GetMin() const
+		{
+			return _Min;
+		}
 
-const Vector3& AxisAlignedBoundingBox::GetMax() const
-{
-	return _Max;
-}
+		const Vector3& AxisAlignedBoundingBox::GetMax() const
+		{
+			return _Max;
+		}
 
-void AxisAlignedBoundingBox::SetMin(_In_ const Vector3& Min)
-{
-	_Min = Min;
-}
+		void AxisAlignedBoundingBox::SetMin(_In_ const Vector3& InMin)
+		{
+			_Min = InMin;
+		}
 
-void AxisAlignedBoundingBox::SetMax(_In_ const Vector3& Max)
-{
-	_Max = Max;
+		void AxisAlignedBoundingBox::SetMax(_In_ const Vector3& InMax)
+		{
+			_Max = InMax;
+		}
+
+		void AxisAlignedBoundingBox::Extend(_In_ const Vector3& InPosition)
+		{
+			_Min = Min(_Min, InPosition);
+			_Max = Max(_Max, InPosition);
+		}
+	}
 }
